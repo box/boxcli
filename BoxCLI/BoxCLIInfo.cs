3bd0445007d19bd249b1f97b21655f3be469ae29
AM Grobelny <allenmichaelgrobelny@gmail.com>
2018-04-11 13:54:01 -0500

    Bumping version 1.2.0 for relase

--- BoxCLI/BoxCLIInfo.cs
+++ BoxCLI/BoxCLIInfo.cs
@@ -4,6 +4,6 @@ namespace BoxCLI
     {
         public const string ProductTitle = "Box CLI";
 
-        public const string Version = "1.1.1";
+        public const string Version = "1.2.0";
     }
 }
