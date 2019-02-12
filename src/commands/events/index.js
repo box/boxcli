f27f716dc962eff04d9860c8130a958937a4ce91
Matt Willer <mwiller@box.com>
2019-02-11 19:52:52 -0800

    Ensure that events command has default time bounds

--- src/commands/events/index.js
+++ src/commands/events/index.js
@@ -2,6 +2,10 @@
 
 const BoxCommand = require('../../box-command');
 const { flags } = require('@oclif/command');
+const date = require('date-fns');
+
+const DEFAULT_START_TIME = '-5d';
+const DEFAULT_END_TIME = 'now';
 
 class EventsGetCommand extends BoxCommand {
 	async run() {
@@ -11,26 +15,26 @@ class EventsGetCommand extends BoxCommand {
 		if (flags.enterprise) {
 			options.stream_type = 'admin_logs';
 		}
-		if (flags['stream-position']) {
-			options.stream_position = flags['stream-position'];
-		}
 		if (flags.limit) {
-			options.limit = parseInt(flags.limit, 10);
-		}
-		if (flags['created-after']) {
-			options.created_after = this.getDateFromString(flags['created-after']);
-		}
-		if (flags['created-before']) {
-			options.created_before = this.getDateFromString(flags['created-before']);
+			options.limit = flags.limit;
 		}
 		if (flags['event-types']) {
 			options.event_type = flags['event-types'];
 		}
 
-		// Default values for backward compatibility
-		if (!options.stream_position && !options.created_before && !options.created_after) {
-			options.created_after = this.getDateFromString('-5d');
-			options.created_before = this.getDateFromString('0d');
+		if (flags['stream-position']) {
+			options.stream_position = flags['stream-position'];
+		} else if (options.stream_type === 'admin_logs') {
+			options.created_before = this.getDateFromString(flags['created-before'] || DEFAULT_END_TIME);
+
+			if (flags['created-before'] && !flags['created-after']) {
+				// If the user specified an end time but no start time, recompute the default start time as
+				// the specified end time minus five days
+				let endTime = date.addDays(date.parse(options.created_before), -5);
+				options.created_after = endTime.toISOString().replace(/\.\d{3}Z$/u, '-00:00');
+			} else {
+				options.created_after = this.getDateFromString(flags['created-after'] || DEFAULT_START_TIME);
+			}
 		}
 
 		let events = await this.client.events.get(options);
@@ -49,14 +53,28 @@ EventsGetCommand.flags = {
 		description: 'Get enterprise events'
 	}),
 	'created-after': flags.string({
-		description: 'Return enterprise events that occured after a time. Use a timestamp or shorthand syntax 0t, like 5w for 5 weeks. If not used, defaults to 5 days ago',
+		description: 'Return enterprise events that occured after a time. Use a timestamp or shorthand syntax 0t, like 5w for 5 weeks. If not used, defaults to 5 days before the end date',
+		exclusive: ['stream_position'],
+		dependsOn: ['enterprise'],
 	}),
 	'created-before': flags.string({
 		description: 'Return enterprise events that occured before a time. Use a timestamp or shorthand syntax 0t, like 5w for 5 weeks. If not used, defaults to now',
+		exclusive: ['stream_position'],
+		dependsOn: ['enterprise'],
 	}),
-	'event-types': flags.string({ description: 'Return enterprise events filtered by event types. Format using a comma delimited list: NEW_USER,DELETE_USER,EDIT_USER' }),
-	'stream-position': flags.string({ description: 'The location in the event stream from which you want to start receiving events' }),
-	limit: flags.string({ description: 'The maximum number of items to return' })
+	'event-types': flags.string({
+		description: 'Return enterprise events filtered by event types. Format using a comma delimited list: NEW_USER,DELETE_USER,EDIT_USER',
+	}),
+	'stream-position': flags.string({
+		description: 'The location in the event stream from which you want to start receiving events',
+		exclusive: [
+			'created-before',
+			'created-after'
+		]
+	}),
+	limit: flags.integer({
+		description: 'The maximum number of items to return',
+	}),
 };
 
 
--- test/commands/events.test.js
+++ test/commands/events.test.js
@@ -4,8 +4,20 @@ const { test } = require('@oclif/test');
 const { assert } = require('chai');
 const { getFixture, TEST_API_ROOT } = require('../helpers/test-helper');
 const leche = require('leche');
+const sinon = require('sinon');
+
+const sandbox = sinon.createSandbox();
 
 describe('Events', () => {
+
+	beforeEach(() => {
+		// Start clocks at 2018-07-13T12:00:00 UTC for all tests
+		sandbox.useFakeTimers(1531508400000);
+	});
+
+	afterEach(() => {
+		sandbox.verifyAndRestore();
+	});
 
 	leche.withData([
 		'events',
@@ -66,8 +78,70 @@ describe('Events', () => {
 					assert.equal(ctx.stdout, jsonOutput);
 				});
 
-			// @TODO(2018-08-20): Add test for default created_before and created_after values
+			test
+				.nock(TEST_API_ROOT, api => api
+					.get('/2.0/events')
+					.query({
+						created_before: '2018-07-13T19:00:00-00:00',
+						created_after: '2018-07-08T19:00:00-00:00',
+						stream_type: 'admin_logs'
+					})
+					.reply(200, fixture)
+				)
+				.stdout()
+				.command([
+					command,
+					'--enterprise',
+					'--json',
+					'--token=test'
+				])
+				.it('should use default time window when no time bound flags are passed', ctx => {
+					assert.equal(ctx.stdout, jsonOutput);
+				});
 
+			test
+				.nock(TEST_API_ROOT, api => api
+					.get('/2.0/events')
+					.query({
+						created_before: '2019-02-11T12:34:56-00:00',
+						created_after: '2019-02-06T12:34:56-00:00',
+						stream_type: 'admin_logs'
+					})
+					.reply(200, fixture)
+				)
+				.stdout()
+				.command([
+					command,
+					'--enterprise',
+					'--created-before=2019-02-11T12:34:56-00:00',
+					'--json',
+					'--token=test'
+				])
+				.it('should set start time to five days before end time when only end time is passed', ctx => {
+					assert.equal(ctx.stdout, jsonOutput);
+				});
+
+			test
+				.nock(TEST_API_ROOT, api => api
+					.get('/2.0/events')
+					.query({
+						created_before: '2018-07-13T19:00:00-00:00',
+						created_after: '2018-01-01T12:34:56-00:00',
+						stream_type: 'admin_logs'
+					})
+					.reply(200, fixture)
+				)
+				.stdout()
+				.command([
+					command,
+					'--enterprise',
+					'--created-after=2018-01-01T12:34:56-00:00',
+					'--json',
+					'--token=test'
+				])
+				.it('should set end time to now when only start time is passed', ctx => {
+					assert.equal(ctx.stdout, jsonOutput);
+				});
 		});
 	});
 
