8067e7a4d6cd23cf8afbfac9ee343f9405e53f80
Matthew Willer <mwiller@box.com>
2019-04-17 15:50:58 -0700

    Convert remaining Collaboration command to use module

--- src/commands/files/collaborations/add.js
+++ src/commands/files/collaborations/add.js
@@ -3,15 +3,24 @@
 const BoxCommand = require('../../../box-command');
 const { flags } = require('@oclif/command');
 const CollaborationsCreateCommand = require('../../collaborations/create');
+const CollaborationModule = require('../../../modules/collaboration');
 
 class FilesCollaborationsAddCommand extends BoxCommand {
-	run() {
-		const { args } = this.parse(FilesCollaborationsAddCommand);
+	async run() {
+		const { args, flags } = this.parse(FilesCollaborationsAddCommand);
 
 		// Clone the args and insert the "file" type at the correct position for the generic command
-		let argv = this.argv.slice();
-		argv.splice(argv.indexOf(args.id) + 1, 0, 'file');
-		return CollaborationsCreateCommand.run(argv);
+		// let argv = this.argv.slice();
+		// argv.splice(argv.indexOf(args.id) + 1, 0, 'file');
+		// return CollaborationsCreateCommand.run(argv);
+
+		args.itemType = 'file';
+		args.itemID = args.id;
+		delete args.id;
+
+		let collabModule = new CollaborationModule(this.client);
+		let collaboration = await collabModule.createCollaboration(args, flags);
+		await this.output(collaboration);
 	}
 }
 
