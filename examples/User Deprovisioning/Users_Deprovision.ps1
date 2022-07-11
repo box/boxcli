56220530d3dbdb42f7e6561597d1866479e5472d
Mike Grytsai <71635461+mgrytsai@users.noreply.github.com>
2022-07-11 19:43:42 +0200

    new version

--- examples/User Deprovisioning/Users_Deprovision.ps1
+++ examples/User Deprovisioning/Users_Deprovision.ps1
@@ -9,38 +9,24 @@
 #Set Employee List CSV Path
 $EmployeeList = "./Employees_5.csv"
 
-#Transfer user content - default "no"
+#Transfer user content before deletion - "Y" or "N"
 $TransferContent = "Y"
 
 #############################################################################
 
 $EmployeeArchiveFolderID =$null
 
+#TODO: Dynamic EMployee Archive folder
+
 
 # Main function
 Function Start{
     Write-Output "Starting User Deprovisioning script..."
 
-    #Get current user id
-    $User = box users:get --json | ConvertFrom-Json
-    $UserId = $User.id
-
-    #Create a Employee Archive folder in User's Root directory if one does not already exist
-    $RootFolder = box folders:items 0 --sort=name --direction=ASC --json | ConvertFrom-Json
-    ForEach($Result in $RootFolder){
-        if( $Result.name -eq "Employee Archive"){
-            $EmployeeArchiveFolderID = $Result.id
-            Write-Output "Employee Archive folder already exists with folder ID: $($EmployeeArchiveFolderID)"
-        }
-    }
-    if($EmployeeArchiveFolderID -eq $null){
-        $EmployeeArchiveFolderID = box folders:create 0 "Employee Archive" --id-only
-        Write-Output "Created new Employee Archive root folder with ID: $($EmployeeArchiveFolderID)"
-    }
-    
     Try {
         # Get employees json file and convert from CSV to an array of objects
         $Employees = Import-Csv $EmployeeList
+        Write-Output "Importing csv of users to deprovision"
         Write-Output $Employees
 
     }
@@ -49,6 +35,25 @@ Function Start{
         break
     }
 
+    #Get current user id
+    $User = box users:get --json | ConvertFrom-Json
+    $UserId = $User.id
+
+    #Create a "Employee Archive" folder in User's Root directory if one does not already exist
+    #List root folder contents
+    $RootFolder = box folders:items 0 --sort=name --direction=ASC --json | ConvertFrom-Json
+    ForEach($Result in $RootFolder){
+        #Check if "Employee Archive" folder already exists
+        if( $Result.name -eq "Employee Archive"){
+            $EmployeeArchiveFolderID = $Result.id
+            Write-Output "Employee Archive folder already exists with folder ID: $($EmployeeArchiveFolderID)"
+        }
+    }
+    if($EmployeeArchiveFolderID -eq $null){
+        #Create new "Employee Archive" folder if it doens't exist
+        $EmployeeArchiveFolderID = box folders:create 0 "Employee Archive" --id-only
+        Write-Output "Created new Employee Archive root folder with ID: $($EmployeeArchiveFolderID)"
+    }
 
     ForEach($Employee in $Employees){
         #search for employee ID
@@ -57,11 +62,11 @@ Function Start{
         Write-Output "Found Employee $($EmployeeName.name) with User ID: $($EmployeeID)"
 
         if($TransferContent -eq "Y"){
-            #transfer user content to current user's root folder
+            #Transfer users content to current user's root folder before deleting user
             Write-Output "Transfering $($EmployeeName.name) content over to current user's Root folder with name ""$($EmployeeName.email) - $($EmployeeName.name)'s Files and Folders"""
             $NewFolder = box users:transfer-content $EmployeeID $UserId --json | ConvertFrom-Json
 
-            #Move folder to Employee Archive folder
+            #Move transferred folder to "Employee Archive" folder
             $TransferredFolder = $NewFolder.id
             box folders:move $TransferredFolder $EmployeeArchiveFolderID
             Write-Output "Transfered employee content $($EmployeeName.name) with User ID: $($EmployeeID) to Employee Archive Folder"
@@ -70,18 +75,6 @@ Function Start{
         box users:delete $EmployeeID 
         Write-Output "Deleted employee $($EmployeeName.name)"
     }
-
-
-    #Transfer user's content. Check if user has content or not
-
-
-    #Delete User
-
-    #Check if Archieve folder exists under Admin account, if not - create. Move user's content to archieve
-
-    #Root (Archive) -> child (user_name_id)
-
-
 }
 
 Start
