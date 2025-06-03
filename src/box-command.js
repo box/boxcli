ec4207715360cc284574e1cbb573586218379517
HS2020 <70487959+Hawra2020@users.noreply.github.com>
2025-06-03 11:14:47 +0200

    feat: Setup proxy for Box CLI with TS SDK (#577)
    * ci: Setup proxy for box cli with ts sdk
    
    * Formatted code
    
    * Revert "Formatted code"
    
    This reverts commit 4c3a97d85c3c58ccdb571c579c914b6f2b8699af.
    
    * Removed new line
    
    * add spacing before the new function

--- src/box-command.js
+++ src/box-command.js
@@ -1093,7 +1093,7 @@ class BoxCommand extends Command {
 			oauth2Url: 'https://account.box.com/api/oauth2',
 		};
 		if (this.settings.enableProxy) {
-			// Not supported in TS SDK
+			client = client.withProxy(this.settings.proxy);
 		}
 		if (this.settings.apiRootURL) {
 			customBaseURL.baseUrl = this.settings.apiRootURL;
